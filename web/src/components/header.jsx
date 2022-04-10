ed78319953efcf3c9864742a1fc83e3795bb1d12
Phong Phi Gia <tpw284@gmail.com>
2022-04-10 18:38:18 +0700

    Web: update intro page

--- web/src/components/header.jsx
+++ web/src/components/header.jsx
@@ -1,7 +1,45 @@
+
 export const Header = (props) => {
   return (
     <header id="header">
-      <div className="intro">
+      <div
+        style={{ backgroundImage: `url(../img/abstract-two-line.svg)` }}
+        className="home_main"
+      >
+        {/* <img src={AbstractBackground} className="bg" alt="" /> */}
+
+        <div className="content1">
+          <div className="home_main_slogan">
+            <div className="home_main_slogan_line">Dr. Health</div>
+            <div className="home_main_description">
+              Lắng nghe cơ thể bạn,
+              <br />
+              săn sóc sức khỏe cả gia đình
+              <br />
+            </div>
+            <div className="btn_space">
+              <a href="https://app.dr-health.com.vn" className="main_home_btn1">
+                <span>Tìm hiểu ngay</span>
+              </a>
+
+              {/* <a href="#strength" className="main_home_btn2">
+                <span>Tính năng của hệ thống</span>
+              </a>
+
+              <a href="#achievements" className="main_home_btn3">
+                <span>Trải nghiệm người dùng</span>
+              </a> */}
+            </div>
+          </div>
+          {true && (
+            <div className="home_main_pic">
+              <img alt="" />
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* <div className="intro">
         <div className="overlay">
           <div className="container">
             <div className="row">
@@ -22,7 +60,7 @@ export const Header = (props) => {
           </div>
         </div>
       </div>
-      <div className="intro-img" />
+      <div className="intro-img" /> */}
     </header>
   );
-}
+};
