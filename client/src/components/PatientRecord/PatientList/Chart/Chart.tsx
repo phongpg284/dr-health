1c2a98ee3d1e4820df604099b693ab321db443b3
khoinguyen12200 <khoi.nguyen12200@gmail.com>
2022-04-14 10:00:44 +0700

    update 8 chart

--- client/src/components/PatientRecord/PatientList/Chart/Chart.tsx
+++ client/src/components/PatientRecord/PatientList/Chart/Chart.tsx
@@ -3,14 +3,16 @@ import { useLazyQuery, useSubscription } from "@apollo/client";
 import React, { useEffect, useState } from "react";
 // import { Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
 
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
-import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, BarElement } from "chart.js";
+import { Line, Bar } from "react-chartjs-2";
 import InputRange from "react-input-range";
 import "react-input-range/lib/css/index.css";
+import { Menu, Dropdown, Button } from "antd";
 
 import "./Chart.scss";
 
 import { GET_INFO_DEVICE, NEW_DEVICE_DATA } from "../schema";
+import { DownOutlined } from "@ant-design/icons";
 
 const vnLegend = {
     spO2: "Chỉ số SpO2",
@@ -206,9 +208,7 @@ const Chart = ({ id, thresholdStatus }: any) => {
         return <span>{(vnLegend as any)?.[value] || (vnBloodPressLegend as any)?.[value]}</span>;
     };
 
-    useEffect(() => {
-        console.log("deviceData", deviceData);
-    }, [deviceData]);
+
     return (
         <div className="patient-info-graph-wrapper">
             {/* <div className="graph_container">
@@ -244,7 +244,7 @@ const Chart = ({ id, thresholdStatus }: any) => {
         </div>
     );
 };
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarElement, Title, Tooltip, Legend);
 
 function ListChart({ deviceData }: { deviceData: any }) {
     const diastole = React.useMemo(() => {
@@ -260,7 +260,8 @@ function ListChart({ deviceData }: { deviceData: any }) {
     return (
         <div className="listChart">
             <ChartDiasAndSys diastole={diastole} systolic={systolic} />
-            <ChartSpO2 spO2={SpO2}/>
+            {/* <SingleLineChart arr={SpO2} title="SpO2" color="coral"/> */}
+            <MultipleChart deviceData={deviceData} />
         </div>
     );
 }
@@ -320,7 +321,7 @@ function ChartDiasAndSys({ diastole, systolic }: { diastole: any[]; systolic: an
     return (
         <div className="myChartWrapper">
             <div className="chartContent">
-                <Line options={options} data={data} />
+                <Line options={options} data={data} height="80vh"/>
                 <div className="rangeWrapper">
                     <InputRange
                         maxValue={max}
@@ -338,9 +339,110 @@ function ChartDiasAndSys({ diastole, systolic }: { diastole: any[]; systolic: an
     );
 }
 
-function ChartSpO2({ spO2 }: { spO2: any[] }) {
+function MultipleChart({ deviceData }: { deviceData: any }) {
+    const arrType = ["SpO2", "Nhiệt độ", "Nhịp tim", "Gương mặt", "Cử chỉ tay", "Giọng nói"];
+    const [type, setType] = useState(arrType[0]);
+
+    const SpO2 = React.useMemo(() => {
+        return deviceData.SpO2.map((item: any) => item.data);
+    }, [deviceData]);
+    const bodyTemp = React.useMemo(() => {
+        return deviceData.bodyTemp.map((item: any) => item.data);
+    }, [deviceData]);
+
+    const heartRate = React.useMemo(() => {
+        return deviceData.heartRate.map((item: any) => item.data);
+    }, [deviceData]);
+
+    const face = React.useMemo(() => {
+        return deviceData.face.map((item: any) => item.data);
+    }, [deviceData]);
+    const armMovement = React.useMemo(() => {
+        const arr = deviceData.armMovement.map((item: any) => item.data);
+        const finalArr = [];
+        let positive = 0;
+        let negative = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === 1) {
+                positive += 1;
+            } else {
+                negative += 1;
+            }
+
+            if ((i + 1) % 5 === 0) {
+                finalArr.push({
+                    positive,
+                    negative,
+                });
+                positive = 0;
+                negative = 0;
+            }
+        }
+        return finalArr;
+    }, [deviceData]);
+    const voice = React.useMemo(() => {
+        const arr = deviceData.voice.map((item: any) => item.data);
+        const finalArr = [];
+        let positive = 0;
+        let negative = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === 1) {
+                positive += 1;
+            } else {
+                negative += 1;
+            }
+
+            if ((i + 1) % 5 === 0) {
+                finalArr.push({
+                    positive,
+                    negative,
+                });
+                positive = 0;
+                negative = 0;
+            }
+        }
+        return finalArr;
+    }, [deviceData]);
+
+
+    const MyMenu = (
+        <Menu>
+            {arrType.map((item: any) => {
+                if (item !== type) {
+                    return (
+                        <Menu.Item>
+                            <div onClick={() => setType(item)}>{item}</div>
+                        </Menu.Item>
+                    );
+                }
+            })}
+        </Menu>
+    );
+
+    return (
+        <div className="myChartWrapper">
+            <div className="chartContent">
+                <div className="dropDownSpace">
+                    <Dropdown overlay={MyMenu} placement="bottomCenter" arrow>
+                        <Button>
+                            {type + " "} <DownOutlined />
+                        </Button>
+                    </Dropdown>
+                </div>
+                {type === arrType[0] && <SingleLineChart arr={SpO2} title={type} color="darkblue" />}
+                {type === arrType[1] && <SingleLineChart arr={bodyTemp} title={type} color="coral" />}
+                {type === arrType[2] && <SingleLineChart arr={heartRate} title={type} color="brown" />}
+                {type === arrType[3] && <FaceChart arr={face} />}
+                {type === arrType[4] && <StackBarChart arr={armMovement} title={type}/>}
+                {type === arrType[5] && <StackBarChart arr={voice} title={type}/>}
+            </div>
+        </div>
+    );
+}
+
+function StackBarChart({ arr, title }: { arr: any[]; title: string }) {
     const min = 0;
-    const max = spO2.length - 1;
+    const max = arr.length - 1;
 
     const [range, setRange] = useState({
         min: 0,
@@ -348,13 +450,91 @@ function ChartSpO2({ spO2 }: { spO2: any[] }) {
     });
 
     React.useEffect(() => {
-        if (spO2.length > 100) {
+        if (arr.length > 100) {
             setRange({
                 min: max - 100,
                 max: max,
             });
         }
-    }, [spO2]);
+    }, [arr]);
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top" as const,
+            },
+            title: {
+                display: true,
+                text: title,
+            },
+        },
+        scales: {
+            x: {
+                stacked: true,
+            },
+            y: {
+                stacked: true,
+            },
+        },
+    };
+
+    const arrSlice = React.useMemo(() => {
+        return arr.slice(range.min, range.max);
+    }, [arr, range]);
+
+    const data = {
+        labels: arrSlice.map(() => ""),
+        datasets: [
+            {
+                label: "Đúng",
+                backgroundColor: "lightskyblue",
+                data: arrSlice.map((item: any) => item.positive),
+            },
+            {
+                label: "Sai",
+                backgroundColor: "lightpink",
+                data: arrSlice.map((item: any) => -item.negative),
+            },
+        ],
+    };
+
+    return (
+        <div>
+            <Bar options={options} data={data} height="80vh"/>
+            <div className="rangeWrapper">
+                <InputRange
+                    maxValue={max}
+                    minValue={min}
+                    value={range}
+                    onChange={(value) => {
+                        if (typeof value !== "number") {
+                            setRange(value);
+                        }
+                    }}
+                />
+            </div>
+        </div>
+    );
+}
+
+function FaceChart({ arr }: { arr: any[] }) {
+    const min = 0;
+    const max = arr.length - 1;
+
+    const [range, setRange] = useState({
+        min: 0,
+        max: max,
+    });
+
+    React.useEffect(() => {
+        if (arr.length > 100) {
+            setRange({
+                min: max - 100,
+                max: max,
+            });
+        }
+    }, [arr]);
 
     const options = {
         responsive: true,
@@ -365,38 +545,99 @@ function ChartSpO2({ spO2 }: { spO2: any[] }) {
         },
     };
 
-    const sp02Slice = React.useMemo(() => {
-        return spO2.slice(range.min, range.max);
-    }, [spO2, range]);
+    const arrSlice = React.useMemo(() => {
+        return arr.slice(range.min, range.max);
+    }, [arr, range]);
 
     const data = {
-        labels: sp02Slice.map(() => ""),
+        labels: arrSlice.map(() => ""),
         datasets: [
             {
-                label: "SpO2",
-                data: sp02Slice,
-                borderColor: "chocolate",
-                backgroundColor: "coral",
+                label: "Gương mặt",
+                data: arrSlice,
+                backgroundColor: "rgba(53, 162, 235, 0.5)",
             },
         ],
     };
 
     return (
-        <div className="myChartWrapper">
-            <div className="chartContent">
-                <Line options={options} data={data} />
-                <div className="rangeWrapper">
-                    <InputRange
-                        maxValue={max}
-                        minValue={min}
-                        value={range}
-                        onChange={(value) => {
-                            if (typeof value !== "number") {
-                                setRange(value);
-                            }
-                        }}
-                    />
-                </div>
+        <div>
+            <Bar options={options} data={data} height="80vh"/>
+            <div className="rangeWrapper">
+                <InputRange
+                    maxValue={max}
+                    minValue={min}
+                    value={range}
+                    onChange={(value) => {
+                        if (typeof value !== "number") {
+                            setRange(value);
+                        }
+                    }}
+                />
+            </div>
+        </div>
+    );
+}
+
+function SingleLineChart(props: { arr: any[]; title: string; color: string }) {
+    const { arr, title, color } = props;
+
+    const min = 0;
+    const max = arr.length - 1;
+
+    const [range, setRange] = useState({
+        min: 0,
+        max: max,
+    });
+
+    React.useEffect(() => {
+        if (arr.length > 100) {
+            setRange({
+                min: max - 100,
+                max: max,
+            });
+        }
+    }, [arr]);
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top" as const,
+            },
+        },
+    };
+
+    const arrSlice = React.useMemo(() => {
+        return arr.slice(range.min, range.max);
+    }, [arr, range]);
+
+    const data = {
+        labels: arrSlice.map(() => ""),
+        datasets: [
+            {
+                label: title,
+                data: arrSlice,
+                backgroundColor: color,
+                borderColor: color,
+            },
+        ],
+    };
+
+    return (
+        <div>
+            <Line options={options} data={data} height="80vh" />
+            <div className="rangeWrapper">
+                <InputRange
+                    maxValue={max}
+                    minValue={min}
+                    value={range}
+                    onChange={(value) => {
+                        if (typeof value !== "number") {
+                            setRange(value);
+                        }
+                    }}
+                />
             </div>
         </div>
     );
