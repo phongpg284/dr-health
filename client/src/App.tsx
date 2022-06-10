672beffd5b52e8397af29fb112d85708a93795e6
Phong Phi Gia <tpw284@gmail.com>
2022-06-11 00:21:11 +0700

    Client:fix scroll carousel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import Main from "pages/Main";
 import LoginPage from "./pages/Login/LoginPage";
 import SignupPage from "./pages/Signup/SignupPage";
 import { Home } from "components/Home";
+import FirstAid2 from "pages/FirstAid2";
 import * as GreetingBotStore from "./app/GreetingBot";
 
 const News = lazy(() => import("pages/News"));
@@ -24,7 +25,7 @@ const CalendarPage = lazy(() => import("pages/Calendar/CalendarPage"));
 const ProfilePage = lazy(() => import("pages/Profile/ProfilePage"));
 const ThresholdPage = lazy(() => import("pages/Threshold/ThresholdPage"));
 const FirstAid = lazy(() => import("pages/FirstAid"));
-const FirstAid2 = lazy(() => import("pages/FirstAid2"));
+// const FirstAid2 = lazy(() => import("pages/FirstAid2"));
 const StrokePoint = lazy(() => import("pages/StrokePoint"));
 const UploadBlood = lazy(() => import("pages/UploadBlood"));
 const ProjectionPhoto = lazy(() => import("pages/ProjectionPhoto"));
@@ -56,54 +57,54 @@ function App() {
         <div className="App">
             <Suspense fallback={<Spin />}>
                 <FooterContext.Provider value={footerRef}>{hasRunEffect && <MyRouter />}</FooterContext.Provider>
-            </Suspense>
+            </Suspense>        
         </div>
     );
 }
 
 function MyRouter() {
-    const { role } = useAppSelector((state) => state.account);
-    const [isAuth, setIsAuth] = useState(true);
-    const [route, setRoute] = useState("/");
+  const { role } = useAppSelector((state) => state.account);
+  const [isAuth, setIsAuth] = useState(true);
+  const [route, setRoute] = useState("/");
 
-    return (
-        <BrowserRouter>
-            {!isAuth && route !== "/" && <Redirect to="/login" />}
+  return (
+    <BrowserRouter>
+      {!isAuth && route !== "/" && <Redirect to="/login" />}
 
-            <Switch>
-                <Route path="/news" component={News} />
-                <Route path="/ho-tro" component={Addition} />
+      <Switch>
+        <Route path="/news" component={News} />
+        <Route path="/ho-tro" component={Addition} />
 
-                <Main path="/co-so-dieu-tri" component={HospitalMap} />
+        <Main path="/co-so-dieu-tri" component={HospitalMap} />
 
-                <Route exact path="/login" component={LoginPage} />
-                <Route exact path="/signup" component={SignupPage} />
-                <Main exact path="/" component={Home} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/signup" component={SignupPage} />
+        <Main exact path="/" component={Home} />
 
-                <Main exact path="/phuc-hoi" component={FirstAid} />
-                <Main exact path="/so-cuu" component={FirstAid2} />
+        <Main exact path="/phuc-hoi" component={FirstAid} />
+        <Main exact path="/so-cuu" component={FirstAid2} />
 
-                <PrivateRoute exact path="/minigame" component={Minigame} />
-                {role === "patient" && <PrivateRoute exact path="/add-games" component={AddMinigame} />}
-                <PrivateRoute exact path="/profile" component={ProfilePage} />
-                <PrivateRoute exact path="/calendar" component={CalendarPage} />
-                <Route exact path="/notifications" component={NotificationsPage} />
-                <PrivateRoute exact path="/threshold/:id" component={ThresholdPage} />
-                {role === "doctor" && (
-                    <>
-                        <PrivateRoute exact path="/patients" component={PatientCardsList} />
-                        <PrivateRoute exact path="/patients/:id" component={PatientList} />
-                        <PrivateRoute exact path="/record" component={DoctorRecord} />
-                        <PrivateRoute exact path="/stroke_point" component={StrokePoint} />
-                        <PrivateRoute exact path="/upload/blood" component={UploadBlood} />
-                        <PrivateRoute exact path="/projection_photo" component={ProjectionPhoto} />
-                    </>
-                )}
-                {role === "patient" && <PrivateRoute exact path="/record" component={PatientRecord} />}
-                {!role && <PrivateRoute exact path="/record" component={PatientRecord} />}
-            </Switch>
-        </BrowserRouter>
-    );
+        <PrivateRoute exact path="/minigame" component={Minigame} />
+        {role === "patient" && <PrivateRoute exact path="/add-games" component={AddMinigame} />}
+        <PrivateRoute exact path="/profile" component={ProfilePage} />
+        <PrivateRoute exact path="/calendar" component={CalendarPage} />
+        <Route exact path="/notifications" component={NotificationsPage} />
+        <PrivateRoute exact path="/threshold/:id" component={ThresholdPage} />
+        {role === "doctor" && (
+          <>
+            <PrivateRoute exact path="/patients" component={PatientCardsList} />
+            <PrivateRoute exact path="/patients/:id" component={PatientList} />
+            <PrivateRoute exact path="/record" component={DoctorRecord} />
+            <PrivateRoute exact path="/stroke_point" component={StrokePoint} />
+            <PrivateRoute exact path="/upload/blood" component={UploadBlood} />
+            <PrivateRoute exact path="/projection_photo" component={ProjectionPhoto} />
+          </>
+        )}
+        {role === "patient" && <PrivateRoute exact path="/record" component={PatientRecord} />}
+        {!role && <PrivateRoute exact path="/record" component={PatientRecord} />}
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
