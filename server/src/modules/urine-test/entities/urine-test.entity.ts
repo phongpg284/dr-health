7fe1de46494c586a734554df32f623a2a2d1fb19
cuongtq17 <cuong.thach@cerebral.com>
2023-12-05 17:18:09 +0700

    add urin stats properties

--- server/src/modules/urine-test/entities/urine-test.entity.ts
+++ server/src/modules/urine-test/entities/urine-test.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, OneToOne } from '@mikro-orm/core';
+import { Entity, OneToOne, Property } from '@mikro-orm/core';
 import { MedicalRecord } from 'src/modules/medical-record/entities/medical-record.entity';
 import { BaseEntity } from 'src/utils/BaseEntity';
 
@@ -6,4 +6,37 @@ import { BaseEntity } from 'src/utils/BaseEntity';
 export class UrineTest extends BaseEntity {
   @OneToOne()
   medicalRecord: MedicalRecord;
+
+  @Property()
+  patientIdOnUrineDevice: string;
+
+  @Property()
+  gluValue: string;
+
+  @Property()
+  leuValue: string;
+
+  @Property()
+  nitValue: string;
+
+  @Property()
+  uroValue: string;
+
+  @Property()
+  proValue: string;
+
+  @Property()
+  phValue: string;
+
+  @Property()
+  bdValue: string;
+
+  @Property()
+  sgValue: string;
+
+  @Property()
+  ketValue: string;
+
+  @Property()
+  billValue: string;
 }
